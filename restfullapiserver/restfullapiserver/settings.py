da97c661a7c512192e591d4d3cd2247e827db679
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-24 14:17:30 +0900

    set project in settings.py

--- restfullapiserver/restfullapiserver/settings.py
+++ restfullapiserver/restfullapiserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-94cb^#k+xa0uny#lyfn@1ososx-y^#)qto3-=e4ko4lg2%2sk#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-KR'
 
 TIME_ZONE = 'UTC'
 
